c0daaf1cb076498afc2638e7caa1dcc5621467b9
jiangjs <421582841@qq.com>
2023-03-15 06:48:25 +0800

    【新增】AOP中的jdk动态代理

---
+++ proxy/src/main/java/com/jiashn/proxy/proxy/JDKProxy.java
@@ -0,0 +1,38 @@
+package com.jiashn.proxy.proxy;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author jiangjs
+ * @date 2023-03-15 6:14
+ * @description 实现JDK的动态代理
+ */
+@Slf4j
+public class JDKProxy {
+    interface Foo{
+        void foo();
+    };
+    static class Target implements Foo{
+
+        @Override
+        public void foo() {
+            log.info("foo method......");
+        }
+    }
+    public static void main(String[] args) {
+        Target target = new Target();
+        //用来加载在运行期间动态生成的字节码
+        ClassLoader classLoader = JDKProxy.class.getClassLoader();
+        Foo o = (Foo)Proxy.newProxyInstance(classLoader, new Class[]{Foo.class}, (p, method, args1) -> {
+            log.info("实现调用方法前增强......");
+            Object result = method.invoke(target, args1);
+            log.info("实现调用方法后增强......");
+            return result;
+        });
+        o.foo();
+    }
+}
